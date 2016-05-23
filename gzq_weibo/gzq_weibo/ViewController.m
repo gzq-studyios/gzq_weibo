74528cd1f9bc2c7b831cfa4d9700a9b4b14ff936
郭兆乾 <gzq_studyios@163.com>
2016-05-23 10:38:13 +0800

    初始化项目

---
+++ gzq_weibo/gzq_weibo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  gzq_weibo
+//
+//  Created by 郭兆乾 on 16/5/23.
+//  Copyright © 2016年 郭兆乾. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ gzq_weibo/gzq_weibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gzq_weibo
+//
+//  Created by 郭兆乾 on 16/5/23.
+//  Copyright © 2016年 郭兆乾. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
